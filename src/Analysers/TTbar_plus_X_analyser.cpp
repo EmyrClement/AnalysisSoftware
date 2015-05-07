bf1a20afa61afdcc4d3684eb1f130c395d42acb4
Douglas Burns <db0268@bristol.ac.uk>
2015-05-07 17:43:31 +0100

    hope it works

--- src/Analysers/TTbar_plus_X_analyser.cpp
+++ src/Analysers/TTbar_plus_X_analyser.cpp
@@ -9,7 +9,6 @@
 #include "../../interface/BTagWeight.h"
 #include "../../interface/GlobalVariables.h"
 #include "../../interface/Event.h"
-#include "../Rochester/NeutrinoSolver.cc" // WHY???
 
 
 namespace BAT {
@@ -49,7 +48,7 @@ void TTbar_plus_X_analyser::ePlusJetsSignalAnalysis(const EventPtr event) {
 
 		histMan_->setCurrentBJetBin(numberOfBjets);
 		histMan_->H1D("BTagWeights")->Fill(bjetWeight);
-		histMan_->H1D("N_BJets_reweighted")->Fill(numberOfBjets, event->weight() );//SAME????????
+		histMan_->H1D("N_BJets_reweighted")->Fill(numberOfBjets, event->weight() );
 		histMan_->H1D("N_BJets")->Fill(numberOfBjets, event->weight());
 
 
@@ -665,30 +664,6 @@ void TTbar_plus_X_analyser::createHistograms() {
 	treeMan_->addBranch("ElectronDown", "F", "FitVariables" + Globals::treePrefix_);
 	treeMan_->addBranch("MuonUp", "F", "FitVariables" + Globals::treePrefix_);
 	treeMan_->addBranch("MuonDown", "F", "FitVariables" + Globals::treePrefix_);
-
-
-
-
-
-
-
-	// treeMan_->addBranch("NeutrinoPossibility1_Px", "F", "FitVariables" + Globals::treePrefix_);
-	// treeMan_->addBranch("NeutrinoPossibility1_Py", "F", "FitVariables" + Globals::treePrefix_);
-	// treeMan_->addBranch("NeutrinoPossibility1_Pz", "F", "FitVariables" + Globals::treePrefix_);
-	// treeMan_->addBranch("NeutrinoPossibility1_Energy", "F", "FitVariables" + Globals::treePrefix_);
-	// treeMan_->addBranch("NeutrinoPossibility1_ChiSq", "F", "FitVariables" + Globals::treePrefix_);
-
-	// treeMan_->addBranch("NeutrinoPossibility2_Px", "F", "FitVariables" + Globals::treePrefix_);
-	// treeMan_->addBranch("NeutrinoPossibility2_Py", "F", "FitVariables" + Globals::treePrefix_);
-	// treeMan_->addBranch("NeutrinoPossibility2_Pz", "F", "FitVariables" + Globals::treePrefix_);
-	// treeMan_->addBranch("NeutrinoPossibility2_Energy", "F", "FitVariables" + Globals::treePrefix_);
-	// treeMan_->addBranch("NeutrinoPossibility2_ChiSq", "F", "FitVariables" + Globals::treePrefix_);
-
-
-
-
-
-
 
 	treeMan_->setCurrentFolder(histogramFolder_ + "/EPlusJets/QCD non iso e+jets");
 	treeMan_->addBranch("HT", "F", "FitVariables" + Globals::treePrefix_);
